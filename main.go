c2aa1c4d9d8f4966dfd19b2c001dc29a0fc361b6
IamDushu <hey.dushyanth@gmail.com>
2023-10-14 20:45:10 +0530

    Creating and using packages

--- main.go
+++ main.go
@@ -58,15 +58,6 @@ func getFirstNames() []string {
 	return firstNames
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-
-	//returning multiple values
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
